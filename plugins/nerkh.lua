234e0351a1069784d2a0aaa2adccf1c8b1cde272
ddimon <dimon3star@gmail.com>
2016-06-12 14:19:18 +0430

    Update nerkh.lua

--- plugins/nerkh.lua
+++ plugins/nerkh.lua
@@ -2,7 +2,7 @@ do
  function run(msg, matches)
  local reply_id = msg['id']
  local text = 'nerkh'
- local text1 ="  قيمت گروه هاي آنتي اسپم :\n\n\n  ماهيانه سوپرگروه 5000 تومان\n  دو ماهه سوپرگروه9000 تومان\n   سه ماهه سوپرگروه 14000 تومان\n\n —-----------------------------\n\n سفارش : @parsaalemi"
+ local text1 ="  قيمت گروه هاي آنتي اسپم :\n\n\n  ماهيانه سوپرگروه 3000 تومان\n  دو ماهه سوپرگروه6000 تومان\n   سه ماهه سوپرگروه 9000 تومان\n\n —-----------------------------\n\n سفارش : @MaStErPoKeR"
    reply_msg(reply_id, text1, ok_cb, false)
  end
  return {
